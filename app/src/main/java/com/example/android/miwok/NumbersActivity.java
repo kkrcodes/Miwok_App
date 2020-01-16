2f09c18d036c4c1dd99b2b5ac4d42b47632bd5e4
Krishna Reddy <krishnareddykond@gmail.com>
2020-01-16 16:45:57 +0530

    Migrate numbers list from Array to ArrayList

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -5,13 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class NumbersActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
-        //Add list of words to String array
-        String[] words = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
+        //Store list of words in ArrayList
+        ArrayList<String> words = new ArrayList<>(
+            Arrays.asList("one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"));
+        Log.v("NumbersActivity", "The ArrayList contains " + words.size() + " words.");
+        for(int i = 0; i < words.size(); i++)
+            Log.v("NumbersActivity", "The word at index " + i + " is: " + words.get(i));
     }
 }
